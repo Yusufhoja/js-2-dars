7b66fa3f3e1323e59b91257d7ed2b7b5a358823f
Yusufhoja <usufhozah@gmail.com>
2024-02-24 14:06:46 +0500

    js 2 dars

---
+++ sctipt.js
@@ -0,0 +1,9 @@
+let a=prompt("String typidagi malumot kiriting")
+let b=+prompt("Number typidagi malumot kiriting")
+let c=prompt("Booleam typidagi malumot kiriting")
+let d=confirm("Hamma malumotlarni togri kiritganingizga ishonchiz komilmi");
+alert(`
+siz kiritgan ${a} ${typeof a}
+siz kiritgan ${b} ${typeof b}
+siz kiritgan ${c} ${typeof c}
+`)
